f54a638c6f9ccd2fe9b050b5ce07bc549cf3ffae
FabianSaldierna <fsaldierna@banxico.org.mx>
2025-01-22 03:05:46 -0600

    added resume page

--- challenge12/src/pages/Resume.jsx
+++ challenge12/src/pages/Resume.jsx
@@ -1,15 +1,31 @@
+import Footer from '../components/Footer';
+import resume from '../assets/resume.pdf';
+
 export default function Resume() {
     return (
         <div>
-            <h2>Mi resume</h2>
+            <h2>My resume</h2>
             <p>
-                Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-                Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-                mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-                lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-                imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-                in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+                Download my <a href={resume}> resume. </a>
             </p>
+            <h3 style={{ textAlign: "left" }}> Front-end knowledge </h3>
+            <ul style={{ textAlign: "left" }}> 
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+
+            </ul>
+            <h3 style={{ textAlign: "left" }}> Back-end knowledge </h3>
+            <ul style={{ textAlign: "left" }}>
+                <li>APIs</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>Postgres</li>
+            </ul>
+
+            <Footer />
         </div>
     );
 }
